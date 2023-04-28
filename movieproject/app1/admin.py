58e1a56ef6ae99f63764a731f5892cedad431101
KIRANVONZULA <kiranvonzula@gmail.com>
2023-04-28 10:48:00 +0530

    firstcommit

---
+++ movieproject/app1/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from app1.models import Movie
+class MovieAdmin(admin.ModelAdmin):
+    list_display=('id','rdate','moviename','hero','heroine','rating')
+admin.site.register(Movie,MovieAdmin)
---
+++ movieproject/app1/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+class Movie(models.Model):
+    rdate=models.DateField()
+    moviename=models.CharField(max_length=20)
+    hero=models.CharField(max_length=20)
+    heroine=models.CharField(max_length=20)
+    rating=models.IntegerField()
